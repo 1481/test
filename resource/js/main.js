cca0fb77f73b5cff05ab9678570f89923457c30a
JacobLinCool <jacoblin2011@gmail.com>
2019-09-06 21:48:14 +0800

    Update main.js

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,3 +1,3 @@
 // Background Music
-var bgm = new Audio("resource/audio/Nocturne in B flat minor, Op. 9 no. 1.mp3");
+var bgm = new Audio("resource/audio/Nocturne in B flat minor, Op. 9 no. 1.mp3?static=1");
 bgm.play();
