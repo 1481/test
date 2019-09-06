ba12755ed6b4a3da42d4d3aae2e901cc20f1e6c3
JacobLinCool <jacoblin2011@gmail.com>
2019-09-06 22:49:21 +0800

    Update main.js

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,3 +1,5 @@
 // Background Music
 var bgm = new Audio("resource/audio/Nocturne in B flat minor, Op. 9 no. 1.mp3?static=1");
+bgm.loop = true;
 bgm.play();
+document.body.addEventListener("touchstart", function(){bgm.play()});
